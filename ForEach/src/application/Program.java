aa6cf3348e96b48236acdb6167df7002ab7fa816
Leianny Poiani <leianny.poiani@gmail.com>
2022-08-02 15:36:00 -0300

    Aulas java sobre arrays, listas, Matrizes, for each e data-hora

---
+++ ForEach/src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		String[] vect = new String[] {"Maria", "Pedro", "joão"};
+		
+		for(String obj : vect) { //para cvada objeto contido no vetor vect, executar:
+			System.out.println(obj);
+		}
+
+	}
+
+}
