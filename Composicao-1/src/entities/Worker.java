673e4b9fbf69e28de1c8cf3cb026a57b66ded605
Leianny Poiani <leianny.poiani@gmail.com>
2022-08-03 11:55:40 -0300

    Exercicio - Enumerações, composição

---
+++ Composicao-1/src/entities/Worker.java
@@ -0,0 +1,91 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import entities.enums.WorkerLevel;
+
+public class Worker {
+	
+	private String name;
+	private WorkerLevel level;
+	private Double baseSalary;
+	
+	private Departament departament;
+	private List<HourContract> contracts = new ArrayList<>(); 
+	//quando é uma operação de "tem muitos", você não inclui ele no construtor, e já instancia na própria declaração.
+	
+	public Worker() {
+	}
+
+	public Worker(String name, WorkerLevel level, Double baseSalary, Departament departament) {
+		this.name = name;
+		this.level = level;
+		this.baseSalary = baseSalary;
+		this.departament = departament;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public WorkerLevel getLevel() {
+		return level;
+	}
+
+	public void setLevel(WorkerLevel level) {
+		this.level = level;
+	}
+
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	public Departament getDepartament() {
+		return departament;
+	}
+
+	public void setDepartament(Departament departament) {
+		this.departament = departament;
+	}
+
+	public List<HourContract> getContracts() {
+		return contracts;
+	}
+	
+	// não tem setContracts pois não podem haver duas listas e ela não pode ser acessada diretamente
+	
+	/**
+	Método responsável por fazer a associação entre um trabalhador e um contrato*/
+	public void addContract(HourContract contract) {
+		contracts.add(contract);
+	}
+	/**
+	Método responsável por desfazer a associação entre um trabalhador e um contrato*/
+	public void removeContract(HourContract contract) {
+		contracts.remove(contract);
+	}
+	
+	public double income(int year, int month) {
+		double sum = baseSalary;
+		Calendar cal = Calendar.getInstance();
+		for(HourContract c : contracts) {
+			int c_year = cal.get(Calendar.YEAR);
+			int c_month = 1 + cal.get(Calendar.MONTH);
+			if(year == c_year && month == c_month) {
+				sum += c.totalValue();
+			}
+		}
+		return sum;
+	}
+
+}
